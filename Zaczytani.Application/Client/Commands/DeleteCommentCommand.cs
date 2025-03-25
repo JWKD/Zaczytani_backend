d5bceb737dffd2d0bc7cc119f06ce8ae9e16b160
Zuzia Bobrykow <bobkrykow.z@gmail.com>
2025-03-25 12:32:49 +0100

    [PT2024ZACZ-178] - change exception type

--- Zaczytani.Application/Client/Commands/DeleteCommentCommand.cs
+++ Zaczytani.Application/Client/Commands/DeleteCommentCommand.cs
@@ -1,6 +1,6 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Identity;
 using Zaczytani.Application.Filters;
+using Zaczytani.Domain.Exceptions;
 using Zaczytani.Domain.Repositories;
 
 namespace Zaczytani.Application.Client.Commands;
@@ -12,14 +12,14 @@ public record DeleteCommentCommand(Guid CommentId) : IRequest, IUserIdAssignable
     {
         UserId = userId;
     }
-    private class Handler(IReviewRepository repository):IRequestHandler<DeleteCommentCommand>
+    private class Handler(IReviewRepository repository) : IRequestHandler<DeleteCommentCommand>
     {
         private readonly IReviewRepository _repository = repository;
         public async Task Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
         {
             var comment = await _repository.GetCommentByIdAsync(request.CommentId, cancellationToken);
-            if(comment==null || comment.UserId!=request.UserId)
-                throw new KeyNotFoundException("Comment not found or you do not have access to it.");
+            if (comment == null || comment.UserId != request.UserId)
+                throw new NotFoundException("Comment not found or you do not have access to it.");
             await _repository.DeleteCommentAsync(request.CommentId, cancellationToken);
             await _repository.SaveChangesAsync(cancellationToken);
 
