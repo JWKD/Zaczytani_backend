8760ba72fa86ec183942cc8b54545257524ab979
Zuzia Bobrykow <bobkrykow.z@gmail.com>
2025-03-26 22:29:39 +0100

    [PT2024ZACZ-180] - add unfollow EP

---
+++ Zaczytani.Application/Client/Commands/UnFollowUserCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Zaczytani.Application.Filters;
+using Zaczytani.Domain.Exceptions;
+using Zaczytani.Domain.Repositories;
+
+namespace Zaczytani.Application.Client.Commands;
+
+public record UnFollowUserCommand(Guid FollowedId) : IRequest, IUserIdAssignable
+{
+    public Guid FollowerId { get; private set; }
+    public void SetUserId(Guid userId)
+    {
+        FollowerId = userId;
+    }
+    private class UnFollowHandler(IUserRepository repository) : IRequestHandler<UnFollowUserCommand>
+    {
+        private readonly IUserRepository _repository = repository;
+        public async Task Handle(UnFollowUserCommand request, CancellationToken cancellationToken)
+        {
+            if (request.FollowerId == request.FollowedId)
+                throw new BadRequestException("User can't unfollow himself");
+            var follow = await _repository.GetFollowAsync(request.FollowerId,request.FollowedId, cancellationToken);
+            if (follow == null)
+                throw new NotFoundException("Follow not found or you do not have access to it.");
+            await _repository.DeleteAsync(request.FollowerId,request.FollowedId, cancellationToken);
+            await _repository.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
--- Zaczytani.Domain/Repositories/IUserRepository.cs
+++ Zaczytani.Domain/Repositories/IUserRepository.cs
@@ -9,6 +9,8 @@ namespace Zaczytani.Domain.Repositories
         IQueryable<User> GetBySearchPhrase(string searchPhrase);
         Task AddAsync(Follower entity, CancellationToken cancellationToken);
         Task<bool> IsFollowingAsync(Guid followerId, Guid followedId, CancellationToken cancellationToken);
+        Task<Follower?> GetFollowAsync(Guid followerId, Guid followedId, CancellationToken cancellationToken);
+        Task DeleteAsync(Guid FollowerId, Guid FollowedId, CancellationToken cancellationToken);
         Task SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
