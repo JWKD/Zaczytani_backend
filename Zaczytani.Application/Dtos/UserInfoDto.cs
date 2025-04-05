87eae019f6cfa0d6bcc07270211a0af9ab51e4f8
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2025-04-05 21:01:16 +0200

    [PT2024ZACZ-193] - Add image url to user info

--- Zaczytani.Application/Dtos/UserInfoDto.cs
+++ Zaczytani.Application/Dtos/UserInfoDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace Zaczytani.Application.Dtos;
 
-public record UserInfoDto(Guid Id);
+public record UserInfoDto(Guid Id, string? ImageUrl);
--- Zaczytani.Application/Shared/Queries/GetUserInfoQuery.cs
+++ Zaczytani.Application/Shared/Queries/GetUserInfoQuery.cs
@@ -1,6 +1,10 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Zaczytani.Application.Dtos;
 using Zaczytani.Application.Filters;
+using Zaczytani.Domain.Entities;
+using Zaczytani.Domain.Exceptions;
+using Zaczytani.Domain.Repositories;
 
 namespace Zaczytani.Application.Shared.Queries;
 
@@ -9,11 +13,15 @@ public class GetUserInfoQuery : IRequest<UserInfoDto>, IUserIdAssignable
     private Guid UserId { get; set; }
     public void SetUserId(Guid userId) => UserId = userId;
 
-    public class GetUserInfoQueryHandler : IRequestHandler<GetUserInfoQuery, UserInfoDto>
+    public class GetUserInfoQueryHandler(UserManager<User> userManager, IFileStorageRepository fileStorageRepository) : IRequestHandler<GetUserInfoQuery, UserInfoDto>
     {
         public async Task<UserInfoDto> Handle(GetUserInfoQuery request, CancellationToken cancellationToken)
         {
-            return new UserInfoDto(request.UserId);
+            var user = await userManager.FindByIdAsync(request.UserId.ToString())
+                ?? throw new NotFoundException("User not found");
+
+            var userImage = fileStorageRepository.GetFileUrl(user.Image);
+            return new UserInfoDto(request.UserId, userImage);
         }
     }
 }
